7c5b3f18ac7b306237e7cd7487a27aedf3dde2ab
Ryan Kubik <rmkubik@me.com>
2023-11-12 14:58:55 -0800

    Add recovery question hints on recover omision

--- src/commands/recover.js
+++ src/commands/recover.js
@@ -2,11 +2,24 @@ import React from "react";
 import Line from "../components/Line";
 import Sequence from "../components/Sequence";
 
+const hints = {
+  1: <Line>PW HINT: A type of severe weather pattern.</Line>,
+  2: (
+    <Line>PW HINT: All vowels should be replaced with special characters.</Line>
+  ),
+  3: <Line>PW HINT: A gaming company.</Line>,
+};
+const hintsUnlocked = {
+  1: false,
+  2: false,
+  3: false,
+};
+
 const RecoveryAnswerOneSeq = () => {
   return (
     <Sequence>
       <Line>Correct!</Line>
-      <Line>PW HINT: A type of severe weather pattern.</Line>
+      {hints[1]}
     </Sequence>
   );
 };
@@ -15,8 +28,7 @@ const RecoveryAnswerTwoSeq = () => {
   return (
     <Sequence>
       <Line>Correct!</Line>
-      <Line>PW HINT: All vowels should be replaced</Line>
-      <Line>with special characters.</Line>
+      {hints[2]}
     </Sequence>
   );
 };
@@ -25,7 +37,7 @@ const RecoveryAnswerThreeSeq = () => {
   return (
     <Sequence>
       <Line>Correct!</Line>
-      <Line>PW HINT: A gaming company.</Line>
+      {hints[3]}
     </Sequence>
   );
 };
@@ -38,24 +50,40 @@ const IncorrectAnswerSeq = () => {
   );
 };
 
+const ShowAnsweredSeq = () => () => {
+  return (
+    <Sequence flattenChildren>
+      <Line>Recovery Question HINTS:</Line>
+      {hintsUnlocked[1] && [hints[1], <Line />]}
+      {hintsUnlocked[2] && [hints[2], <Line />]}
+      {hintsUnlocked[3] && [hints[3], <Line />]}
+    </Sequence>
+  );
+};
+
 const recover =
   (commandLineInterface) =>
   (id, ...answerArgs) => {
     const answer = answerArgs.join(" ").toLowerCase();
 
     switch (true) {
-      case !id:
       case !answer:
+        commandLineInterface.stdout(ShowAnsweredSeq());
+        break;
+      case !id:
       default:
         commandLineInterface.stdout(IncorrectAnswerSeq);
         break;
       case id === "1" && answer === "oregon":
+        hintsUnlocked[1] = true;
         commandLineInterface.stdout(RecoveryAnswerOneSeq);
         break;
       case id === "2" && answer === "the matrix":
+        hintsUnlocked[2] = true;
         commandLineInterface.stdout(RecoveryAnswerTwoSeq);
         break;
       case id === "3" && answer === "toyota":
+        hintsUnlocked[3] = true;
         commandLineInterface.stdout(RecoveryAnswerThreeSeq);
         break;
     }
